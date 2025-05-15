cfa08a5dd8a32375d39d89062adc798bb4e0b8f9
nikolay-rangelov <nikolai.rangeloff@gmail.com>
2025-05-15 21:07:20 +0300

    T03, Ex01 - Browser history rewriten in more efficient way, passing all Judge tests.

---
+++ T03-04_StackAndQueues/src/Lab/Ex01_BrowserHistory_v2.java
@@ -0,0 +1,30 @@
+package Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Ex01_BrowserHistory_v2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+        String input = sc.nextLine();
+        ArrayDeque<String> history = new ArrayDeque<String>();
+        while(!input.equals("Home")){
+            if(input.equals("back")){
+                // back
+                if (history.size() <= 1) {
+                    System.out.println("no previous URLs");
+                } else {
+                    history.pop();
+                    System.out.println(history.peek());
+                }
+            } else {
+                // URL in entered
+                history.push(input);
+                System.out.println(input);
+            }
+            input = sc.nextLine();
+        }
+    }
+}
