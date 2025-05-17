a5744882dd937c30cc4496199498caeae969cd1f
nikolay-rangelov <nikolai.rangeloff@gmail.com>
2025-05-17 17:16:41 +0300

    T04, Ex04 added

--- T03-04_StackAndQueues/src/lab/Ex04_MatchingBrackets.java
+++ T03-04_StackAndQueues/src/lab/Ex04_MatchingBrackets.java
@@ -1,5 +1,7 @@
 package lab;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -7,6 +9,16 @@ import static java.lang.System.in;
 public class Ex04_MatchingBrackets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(in);
-        
+        String input = sc.nextLine();
+        ArrayDeque<Integer> positions = new ArrayDeque<Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                positions.push(i);
+            } else if (input.charAt(i) == ')') {
+                int startPos = positions.pop();
+                int endPos = i;
+                System.out.println(input.substring(startPos, endPos + 1));
+            }
+        }
     }
 }
